946f00281a3654dd9af19c233361270c05fc7c77
AusmitM <ausmitm715@gmail.com>
2025-07-27 14:36:19 -0500

    more changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,25 +11,78 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return ChangeNotifierProvider(
+      create: (context) => AppState(),
+      child: MaterialApp(
+        title: 'Bloom Space',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+        ),
+        home: HomePage(),
+      ),
+    );
+  }
+}
+
+class AppState extends ChangeNotifier {
+  void printButton() {
+    print("Button Pressed");
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: [Introduction()]));
+  }
+}
+
+class Introduction extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<AppState>();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            // style: ElevatedButton.styleFrom(
+            //   backgroundColor: Color.fromARGB(0, 22, 60, 94),
+            // ),
+            onPressed: appState.printButton,
+            child: Text('Join the Community'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Topics extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Topics')),
+      body: Center(
+        child: Text(
+          'Explore various topics related to mental health and well-being.',
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
+
+class Features extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Features')),
+      body: Center(
+        child: Text(
+          'Discover the features of Bloom Space that help you manage your mental health effectively.',
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
