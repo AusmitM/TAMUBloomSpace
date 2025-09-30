dad045c8f69618b3bc345400adf87c4237e21106
AusmitM <ausmitm715@gmail.com>
2025-09-29 22:44:33 -0500

    refactor code and updated structure, start from blank slate

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static const String home = '/home';
+  static const String profile = '/profile';
+  static const String resources = '/resources';
+  static const String counseling = '/counseling';
+  static const String community = '/community';
+  static const String chat1_1 = '/chat1_1';
+}
